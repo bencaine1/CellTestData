47030f77c1a923f844b2993520c68114381f3d84
bencaine1 <bencaine1@gmail.com>
2014-06-10 13:24:22 -0400

    added db functionality

--- getFormInfo.py
+++ getFormInfo.py
@@ -12,25 +12,30 @@ from os.path import isfile, join, getmtime
 from collections import OrderedDict
 import sys
 import re
+import pyodbc
 
 class CellCycle:
-    def __init__(self, cycle_num, cycle_type, cap_charge, cap_discharge, test_req, lot_code, cell_idx):
+    def __init__(self, test_req, lot_code, cell_idx, end_cycle_dts, cycle_num, cycle_type, cap_charge, cap_discharge):
+        self.test_req = test_req
+        self.lot_code = lot_code
+        self.cell_idx = cell_idx
+        self.end_cycle_dts = end_cycle_dts
         self.cycle_num = cycle_num
         self.cycle_type = cycle_type
         self.cap_charge = cap_charge
         self.cap_discharge = cap_discharge
-        self.test_req = test_req
-        self.lot_code = lot_code
-        self.cell_idx = cell_idx
     def __str__(self):
-        s = 'cycle_num: ' + str(self.cycle_num) + '\n'
+        s = 'test_req: ' + str(self.test_req) + '\n'
+        s += 'lot_code: ' + str(self.lot_code) + '\n'
+        s += 'cell_idx: ' + str(self.cell_idx) + '\n'
+        s += 'end_cycle_dts: ' + str(self.end_cycle_dts) + '\n'
+        s += 'cycle_num: ' + str(self.cycle_num) + '\n'
         s += 'cycle_type: ' + str(self.cycle_type) + '\n'
         s += 'cap_charge: ' + str(self.cap_charge) + '\n'
         s += 'cap_discharge: ' + str(self.cap_discharge) + '\n'
-        s += 'test_req: ' + str(self.test_req) + '\n'
-        s += 'lot_code: ' + str(self.lot_code) + '\n'
-        s += 'cell_idx: ' + str(self.cell_idx) + '\n'
         return s
+
+######### SCRAPE ASCII FILES FOR DATA ##########
 
 basePath = 'C:\\Users\\bcaine\\Desktop\\Dummy Maccor Data\\Data\\ASCIIfiles\\TestFiles';
 
@@ -42,6 +47,8 @@ fileListDate = [datetime.fromtimestamp(getmtime(basePath + '\\' + f)).strftime("
 errorFiles=[]
 
 cellCycles = []
+
+sys.stdout.write('Working')
 
 # check last update
 for f in fileList:
@@ -63,11 +70,12 @@ for f in fileList:
         reader = csv.DictReader(myFile, dialect=dialect,delimiter= '\t')
 
         cycle_type = 'Form'
+        # look for test_req and cell_idx in file name
         test_req, cell_idx = None, None
-        test_req_match = re.search('_(?P<number>[0-9]{6})_', f) # test_req: search for 6 numbers flanked by underscores
+        test_req_match = re.search('_(?P<number>[0-9]{6})_', f)
         if test_req_match:
             test_req = test_req_match.group('number')
-        cell_idx_match = re.search('_(?P<number>[0-9]{4})_', f) # cell_idx: search for 4 numbers flanked by underscores
+        cell_idx_match = re.search('_(?P<number>[0-9]{4})[^0-9]', f)
         if cell_idx_match:
             cell_idx = cell_idx_match.group('number')
         
@@ -76,9 +84,11 @@ for f in fileList:
 #        ESlist= []
         chargeCap={}
         dischargeCap={}
+        end_cycle_dts = {}
         cycle = 1
         halfCycle = None
         
+        # FORM02 should contain cycles 2 and 3
         if 'form02' in f.lower():
             cycle += 1
         
@@ -93,20 +103,17 @@ for f in fileList:
             #Full Discharge Condition
             if int(row["ES"])==133 and row["State"]=='D':
                 dischargeCap[cycle]=float(row["Amp-hr"])
+                end_cycle_dts[cycle] = str(row["DPt Time"])
                 if halfCycle==1:
                     halfCycle=None
                     cycle += 1
             #Full Cycle Condition
-                    
-        if 'form01' in f.lower():
-            c1 = CellCycle(1, cycle_type, chargeCap[1], dischargeCap[1], test_req, lot_code, cell_idx)
-            cellCycles.append(c1)
-        elif 'form02' in f.lower():
-            c2 = CellCycle(2, cycle_type, chargeCap[2], dischargeCap[2], test_req, lot_code, cell_idx)
-            cellCycles.append(c2)
-            c3 = CellCycle(3, cycle_type, chargeCap[3], dischargeCap[3], test_req, lot_code, cell_idx)
-            cellCycles.append(c3)
+        
+        for key in chargeCap:
+            c = CellCycle(test_req, lot_code, cell_idx, end_cycle_dts[key], key, cycle_type, chargeCap[key], dischargeCap[key])
+            cellCycles.append(c)
             
+        sys.stdout.write('.')
 #        print fileList[i], "Charge Cap: ", chargeCap, " Discharge Cap: ", dischargeCap
 
     except csv.Error, e:
@@ -120,6 +127,71 @@ for c in cellCycles:
     print c
 
 print "These files didn't process: ", errorFiles
-        
+
+########## ADD TO DB ###########
+
+# connect to db
+cnxn_str =    """
+Driver={SQL Server Native Client 11.0};
+Server=172.16.111.235\SQLEXPRESS;
+Database=CellTestData;
+UID=sa;
+PWD=Welcome!;
+"""
+cnxn = pyodbc.connect(cnxn_str)
+cnxn.autocommit = True
+cursor = cnxn.cursor()
+
+# Populate TestRequest table
+test_req_list = []
+test_req_uid = 1
+for c in cellCycles:
+    if c.test_req not in test_req_list:
+        cursor.execute("""
+        insert into TestRequest(testReq_UID, testReq_num)
+        values (?,?)
+        """, test_req_uid, c.test_req)
+        test_req_list.append(c.test_req)
+        test_req_uid += 1
+# Populate CellAssembly table
+lot_code_list = []
+cell_assy_uid = 1
+for c in cellCycles:
+    if c.lot_code not in lot_code_list:
+        # determine testReq_UID
+        test_req_uid = None
+        row = cursor.execute("""
+        select testReq_UID from TestRequest
+        where testReq_num = ?
+        """, c.test_req).fetchone()
+        if row:
+            test_req_uid = row[0]
+        cursor.execute("""
+        insert into CellAssembly(cellAssy_UID, lotCode, testReq_UID, cell_index)
+        values (?,?,?,?)
+        """, cell_assy_uid, c.lot_code, test_req_uid, c.cell_idx)
+        lot_code_list.append(c.lot_code)
+        cell_assy_uid += 1
+
+# Populate CellCycle table
+cell_cycle_uid = 1
+for c in cellCycles:
+    # determine cellAssy_UID
+    # Should be 3 CellCycle rows for each 1 CellAssembly row
+    # (if both FORM01 and FORM02 present for a given lot code)
+    cell_assy_uid = None
+    row = cursor.execute("""
+    select cellAssy_UID from CellAssembly
+    where lotCode = ?
+    """, c.lot_code).fetchone()
+    if row:
+        cell_assy_uid = row[0]
+    cursor.execute("""
+    insert into CellCycle(cellCycle_UID, cellAssy_UID, endCycle_dts, cycle_num, cycle_type, capacity_charge, capacity_discharge)
+    values (?,?,?,?,?,?,?)
+    """, cell_cycle_uid, cell_assy_uid, c.end_cycle_dts, c.cycle_num, c.cycle_type, c.cap_charge, c.cap_discharge)
+    cell_cycle_uid += 1
+
+
         #for row in spamreader:
             #print ', '.join(row)
